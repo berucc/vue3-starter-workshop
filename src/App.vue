aac2ee28a0f68f9dc2d8620a6df14b3c7e1c9e82
Benjamin Rupp <benjamin.rupp@codecentric.de>
2022-06-10 10:52:53 +0200

    Initial commit ✨
    Setup project using: npm init vue@latest
    
    Remove all unnecessary components and styling to have a clean and simple setup.

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+console.log("Setup App.vue");
+</script>
+
+<template>
+  <h1>Hello World!👋</h1>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+}
+</style>
